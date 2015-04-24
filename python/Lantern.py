ca312f3036e642f1b235d4b8f220f48090675d33
Bruce Gardner <bruce.gardner@sjsu.edu>
2015-04-24 09:57:28 -0700

    Modified Lantern to be OO.

--- python/Lantern.py
+++ python/Lantern.py
@@ -1,32 +1,41 @@
-import serial
+from serial import Serial
 from xbee import ZigBee
+from glob import glob
 
-poles = {1: "\x00\x13\xA2\x00\x40\x8D\x9E\x0E",
-	2: "\x00\x13\xA2\x00\x40\x91\x40\xB6",
-	3: "\x00\x13\xA2\x00\x40\x91\x40\x18",
-	4: "\x00\x13\xA2\x00\x40\x8D\x9E\x5F",
-	5: "\x00\x13\xA2\x00\x40\x91\x40\xB1",
-	6: "\x00\x13\xA2\x00",
-	7: "\x00\x13\xA2\x00",
-	8: "\x00\x13\xA2\x00",
-	9: "\x00\x13\xA2\x00\x40\x8C\xDB\x45"
-}
+class XbeePoleControl:
+	def __init__(self, number):
+		self.number = number
 
-def Open():
-	# SJSU Controller
-	PORT='/dev/tty.usbserial-A600eIdg'
+		self.poles = {
+			1: "\x00\x13\xA2\x00\x40\x8D\x9E\x0E",
+			2: "\x00\x13\xA2\x00\x40\x91\x40\xB6",
+			3: "\x00\x13\xA2\x00\x40\x91\x40\x18",
+			4: "\x00\x13\xA2\x00\x40\x8D\x9E\x5F",
+			5: "\x00\x13\xA2\x00\x40\x91\x40\xB1",
+			6: "\x00\x13\xA2\x00",
+			7: "\x00\x13\xA2\x00",
+			8: "\x00\x13\xA2\x00",
+			9: "\x00\x13\xA2\x00\x40\x8C\xDB\x45"
+		}
 
-	# Steve's Controller
-	# PORT='/dev/tty.usbserial-A600eIcJ'
-	BAUD_RATE = 115200
+		# setup USB port with xbee on it.
+		usb_ports = glob("/dev/tty.usbserial-*")
+		if len(usb_ports) == 1:
+			self.usb_port = usb_ports[0]
+		elif len(usb_ports) == 0:
+			print "no usb devices found. usb port not set."
+		elif len(usb_ports) > 1:
+			print "many usb devices found. usb port not set."
 
-	ser = serial.Serial(PORT, BAUD_RATE)
-	xbee = ZigBee(ser,escaped=True)
+		self.baud = 115200
 
-	return(xbee)
+		self.mac = self.poles[self.number]
+		
 
-def Close():
-	ser.close()
+	def open(self):
+		self.serial = Serial(self.usb_port, self.baud)
+		self.xbee   = ZigBee(self.serial, escaped=True)
 
-def Pole(number):
-	return poles[number]
+	def close(self):
+		self.serial.close()
+
