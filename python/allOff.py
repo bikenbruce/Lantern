576864f2ab61b942c68999e3eb874d9a8865af95
Bruce Gardner <bruce.gardner@sjsu.edu>
2015-06-02 16:04:01 -0700

    updated base.

--- python/allOff.py
+++ python/allOff.py
@@ -2,30 +2,11 @@ import serial
 from xbee import ZigBee
 import time
 
-# SJSU Controller
-PORT='/dev/tty.usbserial-A600eIdg'
-BAUD_RATE = 115200
+from Lantern import XbeePoleControl
 
-ser = serial.Serial(PORT, BAUD_RATE)
-xbee = ZigBee(ser,escaped=True)
+control = XbeePoleControl()
+control.open()
 
-POLE1 = "\x00\x13\xA2\x00\x40\x8D\x9E\x0E"
-POLE2 = "\x00\x13\xA2\x00\x40\x91\x40\xB6"
-POLE3 = "\x00\x13\xA2\x00\x40\x91\x40\x18"
-POLE4 = "\x00\x13\xA2\x00\x40\x8D\x9E\x5F"
-POLE5 = "\x00\x13\xA2\x00\x40\x91\x40\xB1"
-POLE6 = "\x00\x13\xA2\x00"
-POLE7 = "\x00\x13\xA2\x00"
-POLE8 = "\x00\x13\xA2\x00"
+control.send(4, "\x1F")
 
-xbee.send("tx",data="\xff", dest_addr_long=POLE4, dest_addr="\xff\xfe")
-response = xbee.wait_read_frame()
-shot_addr = response["dest_addr"]
-print response
-
-xbee.send("tx",data="\x1f", dest_addr_long=POLE4, dest_addr=shot_addr)
-response = xbee.wait_read_frame()
-print response
-
-    
-ser.close()
+control.close()
--- python/allOn.py
+++ python/allOn.py
@@ -2,32 +2,11 @@ import serial
 from xbee import ZigBee
 import time
 
-# SJSU Controller
-# PORT='/dev/tty.usbserial-A600eIdg'
+from Lantern import XbeePoleControl
 
-# Steve's Controller
-PORT='/dev/tty.usbserial-A600eIcJ'
-BAUD_RATE = 115200
+control = XbeePoleControl()
+control.open()
 
-ser = serial.Serial(PORT, BAUD_RATE)
-xbee = ZigBee(ser,escaped=True)
+control.send(4, "\x20")
 
-POLE1 = "\x00\x13\xA2\x00\x40\x8D\x9E\x0E"
-POLE2 = "\x00\x13\xA2\x00\x40\x91\x40\xB6"
-POLE3 = "\x00\x13\xA2\x00\x40\x91\x40\x18"
-POLE4 = "\x00\x13\xA2\x00\x40\x8D\x9E\x5F"
-POLE5 = "\x00\x13\xA2\x00\x40\x91\x40\xB1"
-POLE6 = "\x00\x13\xA2\x00"
-POLE7 = "\x00\x13\xA2\x00"
-POLE8 = "\x00\x13\xA2\x00"
-
-xbee.send("tx",data="\xff", dest_addr_long=POLE3, dest_addr="\xff\xfe")
-response = xbee.wait_read_frame()
-shot_addr = response["dest_addr"]
-print response
-
-xbee.send("tx",data="\x20", dest_addr_long=POLE3, dest_addr=shot_addr)
-response = xbee.wait_read_frame()
-print response
-
-ser.close()
+control.close()
